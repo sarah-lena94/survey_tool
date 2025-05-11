8c1474284819f7526c61e4f76b93f42913aded7c
SarahWoköck <sarah.wokoeck@advisori.de>
2025-05-11 13:44:35 +0200

    initial commit

---
+++ backend/src/main/java/com/slwokoeck/backend/SurveyToolApplication.java
@@ -0,0 +1,13 @@
+package com.slwokoeck.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyToolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/slwokoeck/backend/SurveyToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slwokoeck.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
